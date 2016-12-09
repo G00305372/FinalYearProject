147a34b1c72105c5a6f7c97b8f8e5beb74fd8195
Stephen Molloy <Stephen Molloy>
2016-12-09 12:22:18 +0000

    git commit

---
+++ Test2/app/src/main/java/com/example/stephenmolloy/test2/Equipment.java
@@ -0,0 +1,29 @@
+package com.example.stephenmolloy.test2;
+
+/**
+ * Created by Stephen Molloy on 07/12/2016.
+ */
+public class Equipment {
+
+    public String model;
+    public double equipment_ID;
+    public String GPS;
+
+    //Constructor
+    public Equipment(String model, double equipment_ID, String GPS)
+    {
+        this.model = model;
+        this.equipment_ID = equipment_ID;
+        this.GPS = GPS;
+    }
+
+    public String getName(){return model;}
+    public void setName(String empname){model = empname;}
+
+    public double getID(){return equipment_ID;}
+    public void setID(double ID){equipment_ID = ID; }
+
+    public String getGPS(){return GPS;}
+    public void setGPS(String location){GPS = location;}
+
+}
